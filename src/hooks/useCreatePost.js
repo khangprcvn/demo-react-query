b3e965a1344236cb25d69a1bdf6a49a45f598744
Do Viet Khang <dvkhang@gvivn.global>
2023-07-02 16:44:43 +0700

    apply replay for creating post

--- src/hooks/useCreatePost.js
+++ src/hooks/useCreatePost.js
@@ -34,5 +34,6 @@ export default function useCreatePost() {
     onError: (error, newTodo, context) => {
       queryClient.setQueryData(['posts'], context.previousPosts);
     },
+    retry: 1,
   });
 }
